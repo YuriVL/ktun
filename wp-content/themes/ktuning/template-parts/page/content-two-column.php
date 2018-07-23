7fbc3ba11aecdd4bfba9aacd75cecacc153e6139
vagrant <vagrant@homestead>
2018-07-23 09:23:12 +0300

    two-column

--- wp-content/themes/ktuning/template-parts/page/content-two-column.php
+++ wp-content/themes/ktuning/template-parts/page/content-two-column.php
@@ -4,26 +4,13 @@
         <div class="normal-title">
             <h3>Мы делаем ремонт авто</h3>
         </div>
-        <div class="text">Автосервис "Ktuning" готов предложить каждому клиенту качественный авторемонт любой сложности.
+        <div class="text">Автосервис "OKtuning" готов предложить каждому клиенту качественный авторемонт любой сложности.
             Также, у нас вы можете всегда получить профессиональную консультацию касательно ремонта, обслуживания и эксплуатации вашего автомобиля.</div>
     </div>
 
     <div class="skills-column col-lg-6 col-md-12 col-xs-12">
         <!--Progress Levels-->
         <div class="progress-levels margin-top-55">
-
-            <!--Skill Box-->
-            <div class="progress-box wow fadeInRight" data-wow-delay="100ms" data-wow-duration="1500ms" style="visibility: hidden; animation-duration: 1500ms; animation-delay: 100ms; animation-name: none;">
-                <div class="box-title"><span class="icon flaticon-transport-1"></span> Ремонт тормозной системы</div>
-                <div class="inner">
-                    <div class="bar">
-                        <div class="bar-innner">
-                            <div class="bar-fill" data-percent="80" style="width: 80%;"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
             <!--Skill Box-->
             <div class="progress-box wow fadeInRight" data-wow-delay="200ms" data-wow-duration="1500ms" style="visibility: hidden; animation-duration: 1500ms; animation-delay: 200ms; animation-name: none;">
                 <div class="box-title"><span class="icon flaticon-gear"></span> Ремонт двигателей</div>
@@ -47,10 +34,20 @@
                     </div>
                 </div>
             </div>
-
+            <!--Skill Box-->
+            <div class="progress-box wow fadeInRight" data-wow-delay="100ms" data-wow-duration="1500ms" style="visibility: hidden; animation-duration: 1500ms; animation-delay: 100ms; animation-name: none;">
+                <div class="box-title"><span class="icon flaticon-transport-1"></span> Ремонт тормозной системы</div>
+                <div class="inner">
+                    <div class="bar">
+                        <div class="bar-innner">
+                            <div class="bar-fill" data-percent="80" style="width: 80%;"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <!--Skill Box-->
             <div class="progress-box wow fadeInRight" data-wow-delay="400ms" data-wow-duration="1500ms" style="visibility: hidden; animation-duration: 1500ms; animation-delay: 400ms; animation-name: none;">
-                <div class="box-title"><span class="icon flaticon-transport-6"></span> Сварка</div>
+                <div class="box-title"><span class="icon flaticon-transport-6"></span> Кузовные работы</div>
                 <div class="inner">
                     <div class="bar">
                         <div class="bar-innner">
