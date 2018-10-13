4f5ab6fda2bddf33efe6521136c4fff7416cb8bf
Yuri Stopchik <yastopch@gmail.com>
2018-10-13 16:41:00 +0300

    1310: header

--- wp-content/themes/ktuning/header.php
+++ wp-content/themes/ktuning/header.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="utf-8">
     <!-- Responsive -->
-	<meta name="keywords" content="Ремонт автомобиля, автосервис, диагностика авто, ремонт двигателя, ремонт подвески, ремонт тормозной системы, ремонт авто, Могилев, область">    
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Ремонт автомобиля, автосервис, диагностика авто, ремонт двигателя, ремонт подвески, ремонт тормозной системы, ремонт авто, Могилев, область">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <!--Favicon-->
     <link rel="shortcut icon" href="<?php echo IMG_DIR ?>/favicon.ico" type="image/x-icon">
     <link rel="icon" href="<?php echo IMG_DIR ?>/favicon.ico" type="image/x-icon">
